4883f7b91cce74f2f80a561113239e2de8f63d50
ROHIT-01234 <120010829+ROHIT-01234@users.noreply.github.com>
2024-01-21 13:56:34 +0530

    Create warshall 2.c

---
+++ warshall 2.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+void floyd(int cost[10][10], int n)
+{
+	for(int k=0;k<n;k++)
+	{
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				cost[i][j]=cost[i][j]||(cost[i][k]&&cost[k][j]);
+				
+		    }
+	    }
+	}
+	printf("All Pairs Shortest Path is :\n");
+		for(int i=0;i<n;i++)
+	    {
+	    	for(int j=0;j<n;j++)
+	    	{
+	    		printf("%d ",cost[i][j]);
+			}
+	    	printf("\n");
+		}
+}
+int main()
+{
+    int n,cost[10][10];
+	printf("Enter the no. of vertices: ");
+	scanf("%d",&n);
+	
+	printf("Enter the values:\n");
+	for(int i=0;i<n;i++)
+	{
+	    for (int j=0;j<n;j++)
+	    {
+	        scanf("%d",&cost[i][j]);
+	    }
+	}
+	
+	printf("Given Matrix: \n");
+	for(int i=0;i<n;i++)
+	{
+	    for (int j=0;j<n;j++)
+	    {
+	       printf("%d\t",cost[i][j]);
+	    }
+	    printf("\n");
+	}
+	
+	floyd(cost,n);
+}

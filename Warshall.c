2bb0970a6eea8d844543e32e76503611d99ff762
ROHIT-01234 <120010829+ROHIT-01234@users.noreply.github.com>
2024-01-21 13:35:34 +0530

    Create Warshall.c

---
+++ Warshall.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void floyd(int a[4][4], int n)
+{
+	for(int k=0;k<n;k++)
+	{
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				a[i][j]=a[i][j]||(a[i][k]&&a[k][j]);
+				
+		    }
+	    }
+	}
+	printf("All Pairs Shortest Path is :\n");
+		for(int i=0;i<n;i++)
+	    {
+	    	for(int j=0;j<n;j++)
+	    	{
+	    		printf("%d ",a[i][j]);
+			}
+	    	printf("\n");
+		}
+}
+int main()
+{
+	int cost[4][4] = {{0, 1, 0, 0}, {0, 0,  0, 1}, {0, 0, 0, 0}, {1, 0, 1, 0} }; 
+	int n = 4;
+
+	floyd(cost,n);
+}
